c0cc0af7cc66e089720b115f6721d595c6896e7b
BloomVault <bloomvaultfarms@gmail.com>
2025-08-10 15:13:53 -0400

    Delete assets directory

--- assets/js/app.js
+++
@@ -1,7 +0,0 @@
-
-import strains from './strains.json' assert { type: 'json' };
-const featured = document.getElementById('featured');
-if (featured){
-  const picks = strains.slice(0,4);
-  featured.innerHTML = picks.map(s => `<div class="card"><h3>${s.name}</h3><p class="small">${s.terpenes}</p><a class="btn" href="strain.html?id=${s.id}">View</a></div>`).join('');
-}
